881e5840c6dd133c093753dff3b9990d8349d6db
suub13 <yousubin98@gmail.com>
2023-09-15 15:30:43 +0900

    docs in 2 columns and auto-overflow

--- LineFormattedData.py
+++ LineFormattedData.py
@@ -17,17 +17,24 @@ class LineFormattedData:
         self.image = image
         self.column_num = column_num
         self.line_formatted_data = self.process_pdf(
-            self.image, self.column_num)
+            self.image)
 
-    def process_pdf(self, image, column_num):
+    def process_pdf(self, image):
 
         extracted_data = self.extract_data_from_image(image, 10)
-        width, height = image.size
+
+        self.width, self.height = image.size
+        # for i in range(len(extracted_data)//4):
+        #     print(extracted_data[i*4])
+        #     print(extracted_data[i*4+1])
+        #     print(extracted_data[i*4+2])
+        #     print(extracted_data[i*4+3])
+        # print(extracted_data)
 
         if self.column_num == 1:
-            line_formatted = self.data_to_text_json_1(extracted_data, height)
+            line_formatted = self.data_to_text_json_1(extracted_data, self.height)
         elif self.column_num == 2:
-            line_formatted = self.data_to_text_json_2(extracted_data, width, height)
+            line_formatted = self.data_to_text_json_2(extracted_data, self.width, self.height)
         else:
             print("Invalid column_num")
 
@@ -72,18 +79,18 @@ class LineFormattedData:
 
         return avg_gap, max_height
 
-    def data_to_text_json_1(self, words, width, height):
+    def data_to_text_json_1(self, words, height):
         spacing, max_height = self.calculate_spacing_height(words)
 
         formatted_text = ""
 
         # set the left and right margin
-        right_x = max([(word['x'] + word['width']) for word in words])
-        left_x = min([word['x'] for word in words])
+        self.right_x = max([(word['x'] + word['width']) for word in words])
+        self.left_x = min([word['x'] for word in words])
 
         # set the top and the top /bottom margin with the minimum y value
-        top_y = min([word['y'] for word in words])
-        bottom_y = height - top_y
+        self.top_y = min([word['y'] for word in words])
+        self.bottom_y = self.height - self.top_y
 
         # 한 줄씩 나누기 위한 parameters
         line_formatted = []
@@ -91,6 +98,8 @@ class LineFormattedData:
         first_y = words[0]['y']
 
         for idx, word in enumerate(words[:-1]):
+            if (word['text'][-1] != '-') & (word['text'][-1] == '-'):
+                word['text'] = word['text'][:-1]
             next_word = words[idx + 1]
             formatted_text += word['text'] + ' '  # 띄움이 확인 불가, spacing 1개로 가정
 
@@ -102,7 +111,7 @@ class LineFormattedData:
             # 블락이 같고 paragraph 도 같은데, 라인이 다르면 뒤에 다음 단어 width 이상 남으면 개행
             elif (word['block_num'] == next_word['block_num']) & (word['par_num'] == next_word['par_num']) \
                     & (word['line_num'] != next_word['line_num']) & \
-                    (right_x - word['x'] - word['width'] > next_word['width']):
+                    (self.right_x - word['x'] - word['width'] > next_word['width']):
                 pass
 
             # 라인이 다른데 다음 줄이 알파벳 으로 시작 하지 않으면 개행
@@ -143,16 +152,16 @@ class LineFormattedData:
         # 가상의 중간 선 보다 작은 것 중 max 는 1단의 right, 중간 선 보다 큰 것 중 max는 2단의 right
         middle_line_x = width / 2
 
-        first_column_left_x = min([word['x'] for word in words])
-        first_column_right_x = max(
+        self.first_column_left_x = min([word['x'] for word in words])
+        self.first_column_right_x = max(
             [(word['x'] + word['width']) for word in words if (word['x'] + word['width']) < middle_line_x])
 
-        second_column_right_x = min(
+        self.second_column_left_x = min(
             [(word['x'] + word['width']) for word in words if (word['x'] + word['width']) > middle_line_x])
-        second_column_right_x = max([(word['x'] + word['width']) for word in words])
+        self.second_column_right_x = max([(word['x'] + word['width']) for word in words])
 
-        top_y = min([word['y'] for word in words])
-        bottom_y = height - top_y
+        self.top_y = min([word['y'] for word in words])
+        self.bottom_y = height - self.top_y
 
         # 한 줄씩 나누기 위한 parameters
         line_formatted = []
@@ -160,14 +169,16 @@ class LineFormattedData:
         first_word_y = words[0]['y']
 
         for idx, word in enumerate(words[:-1]):
+            if (word['text'][-1] != '-') & (word['text'][-1] == '-'):
+                word['text'] = word['text'][:-1]
             next_word = words[idx + 1]
             formatted_text += word['text'] + ' '  # 띄움이 확인 불가, spacing 1개로 가정
 
             # 페이지 중간의 값 보다 작으면 2단 페이지 에서 왼편, 크면 오른 편의 맥스 기준을 지정
             if word['x'] < middle_line_x:
-                criteria = first_column_right_x
+                criteria = self.first_column_right_x
             else:
-                criteria = second_column_right_x
+                criteria = self.second_column_right_x
 
             # 블락이 다를 때 또는 같은데 paragraph 가 다를 경우 개행
             if (word['block_num'] != next_word['block_num']) \
@@ -191,7 +202,7 @@ class LineFormattedData:
             # first column, the gap will be from the word to the bottom + top to the next word
             gap = next_word['y'] - word['y']
             if word['y'] > next_word['y'] | ((word['x'] < middle_line_x) & (next_word['x'] > middle_line_x)):
-                gap = bottom_y - word['y'] + next_word['y'] - top_y
+                gap = self.bottom_y - word['y'] + next_word['y'] - self.top_y - spacing
 
             # add \n until the gap gets less than average height of words
             while gap > avg_height:
@@ -202,6 +213,7 @@ class LineFormattedData:
                         'text': formatted_text,
                         'x': first_word_x,
                         'y': first_word_y,
+
                     })
                     first_word_x = next_word['x']
                     first_word_y = next_word['y']
@@ -216,5 +228,6 @@ class LineFormattedData:
         })
 
         return line_formatted
+
 
 
--- TextInImages.py
+++ TextInImages.py
@@ -1,13 +1,23 @@
 import LineFormattedData
 from pdf2image import convert_from_path
 import time
+from docx import Document
+from docx.shared import Cm, Pt, Inches
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_BREAK
+from docx import Document
+from docx.enum.section import WD_SECTION_START
+from docx.oxml.ns import qn
 
 class TextInImages:
+    # 아래 코드로 바꿔야 함
+    # def __init__(self, image_list, column_num):
+    #     self.image_list = image_list
     def __init__(self, pdf_url, column_num):
         self.pdf_url = pdf_url
         self.image_list = self.pdf2jpg(pdf_url)
         self.column_num = column_num
 
+        # 이거 create_text_list call한 값으로 check_connecting_line 호출하면 될듯
         self.image_extracted_data_list = self.create_text_list(self.image_list, self.column_num)
 
     def pdf2jpg(self, pdf_url):
@@ -19,24 +29,106 @@ class TextInImages:
         return images
 
     def create_text_list(self, images, column_num):
-        image_text_list = []
+        image_list = []
+
         for idx, image in enumerate(images):
             # image_numpy = np.array(image)
-            text_in_image = LineFormattedData.LineFormattedData(image, column_num)
-            line_formatted = text_in_image.line_formatted_data
-            image_text_list.append(line_formatted)
+            text_in_image = LineFormattedData.LineFormattedData(image, column_num[idx])
+            image_list.append(text_in_image)
 
-        return image_text_list
+
+        # 여기서 list를 check_connecting_line에 넣어주면 될듯
+        # 받아오는 값을 image_extracted_data_list에 넣어주면 될듯
+
+
+        return image_list
+
+    def points_to_cm(self, points, height):
+        return Cm(points / height * 29.7)
+
+        # 불러올 때, self.insert_text_to_word(image_list, self.pdf_url[:-4]+".docx")
+    def insert_text_to_word(self, image_list, output_filename):
+        # Create a new Word document
+        doc = Document()
+        sections = doc.sections
+        for idx, page in enumerate(image_list):
+            # 일단은 다음줄 연결 없이 한 장씩 뽑기
+            # Create a new paragraph
+            new_section = doc.add_section(WD_SECTION_START.CONTINUOUS)
+            new_section.start_type
+
+
+            # p1 = doc.add_heading('This is the title!!!!!!!!!!!!!')
+            # p1.alignment = 1
+
+            # Second Section
+            new_section = doc.add_section(WD_SECTION_START.CONTINUOUS)
+            new_section.start_type
+
+            section = sections[2]
+
+            if page.column_num == 1:
+                for section in sections:
+                    section.top_margin = self.points_to_cm(page.top_y, page.height)
+                    section.bottom_margin = self.points_to_cm(page.height - page.bottom_y, page.height)
+                    section.left_margin = self.points_to_cm(page.left_x, page.height)
+                    section.right_margin = self.points_to_cm(page.left_x, page.height)
+
+            elif page.column_num == 2:
+            # # Set to 2 column layout
+                sectPr = section._sectPr
+                cols = sectPr.xpath('./w:cols')[0]
+                cols.set(qn('w:num'), '2')
+
+            # 1440 twip units are 1 inch.
+                space_value = 0.5 * 1440
+                cols.set(qn('w:space'), str(space_value))
+
+                for section in sections:
+                # for section in sections:
+                    section.top_margin = self.points_to_cm(page.top_y, page.height)
+                    section.bottom_margin = self.points_to_cm(page.height - page.bottom_y, page.height)
+                    section.left_margin = self.points_to_cm(page.first_column_left_x, page.height)
+                    section.right_margin = self.points_to_cm(page.first_column_left_x, page.height)
+
+
+            # Third Section
+            # new_section = doc.add_section(WD_SECTION_START.NEW_COLUMN)
+            # new_section.start_type
+
+            # p1 = doc.add_paragraph('This is the 2nd para')
+            # p1.alignment = 1
+
+            # paragraph = doc.add_paragraph()
+            # paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+            for par in page.line_formatted_data:
+                if isinstance(par, dict):
+                    if par['text'].startswith('e '):
+                        p = doc.add_paragraph(par['text'][2:], style='List Bullet')
+                        # p.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+                    # Add text with the specified font size
+                    else:
+                        # run = paragraph.add_run(par['text'])  # Use .get() to handle missing 'text' key
+                        p = doc.add_paragraph(par['text'])
+                        # p.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+                    style = doc.styles['Normal']
+                    font = style.font
+                    font.size = Pt(10)
+
+        doc.save(output_filename)
+
 
 if __name__ == '__main__':
     start = time.time()
 
-    extractor = TextInImages("./test/Meeting Minute3.pdf", 2)
-    print(extractor.pdf_url)
-    for i in extractor.image_extracted_data_list:
-        for j in i:
-            print(j['text'], end="")
-
+    extractor = TextInImages("./test/rt_img.pdf", [2])
+    extractor.insert_text_to_word(extractor.image_extracted_data_list, extractor.pdf_url[:-4] + ".docx")
+    # print(extractor.pdf_url)
+    # for i in extractor.image_extracted_data_list:
+    #     for j in i:
+    #         # print(j['text'], end="")
+    #         print(j)
     end = time.time()
 
     print("\nIt took " + str(end - start) + " seconds")
